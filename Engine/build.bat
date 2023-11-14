d458f2ec243e506f277d9b26bf6d096a5902ad46
DragonH666 <40714112+DragonH666@users.noreply.github.com>
2023-11-14 20:57:49 +0100

    Add files via upload

--- Engine/build.bat
+++ Engine/build.bat
@@ -4,5 +4,5 @@ set code=%cd%
 set workaround=-Wl,-u,___mingw_vsnprintf -Wl,--defsym,___ms_vsnprintf=___mingw_vsnprintf
 pushd ..\build
 ::cl -Zi /Od  -MDd %code%/platform.cpp %code%/glad.c glfw3.lib opengl32.lib user32.lib gdi32.lib shell32.lib /Fe:ClassicGames.exe 
-g++ -O0 %code%/platform.cpp  %code%/glad.o -march=x86-64  %workaround% -L.  -lglfw3 -lgdi32  -o Game.exe
+g++ -O0 %code%/platform.cpp  %code%/glad.o -march=x86-64 %workaround% -static -static-libgcc -static-libstdc++ -L.  -lglfw3 -lgdi32  -o Game.exe
 popd
